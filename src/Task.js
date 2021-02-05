c4d3633da5d7831faf7563df085071aa40a48144
Nel Guerreiro <nelmmg@gmail.com>
2021-02-05 21:58:49 +0000

    Added confirmation dialog

--- src/Task.js
+++ src/Task.js
@@ -4,21 +4,21 @@ import Checkbox from '@material-ui/core/Checkbox';
 import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
 import db, { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import ConfirmDialog from './ConfirmDialog';
 
 function Task({ id, desc, done }) {
 
     const [checked, setChecked] = useState(done);
     const [user] = useAuthState(auth);
+    const [confirmOpen, setConfirmOpen] = useState(false);
 
     const handleChange = (e) => {
         e.preventDefault();
         setChecked(e.target.checked);
     };
 
-    const deleteTask = (e) => {
-        e.preventDefault();
-        setChecked(!checked)
-        db.collection('users').doc(user.uid).collection("tasks").doc(id)
+    const deleteTask = (taskId) => {
+        db.collection('users').doc(user.uid).collection("tasks").doc(taskId)
             .delete();
     }
 
@@ -36,7 +36,15 @@ function Task({ id, desc, done }) {
                 {desc}
             </div>
             <div className="Task__actions">
-                <HighlightOffOutlinedIcon onClick={deleteTask} />
+                <HighlightOffOutlinedIcon onClick={() => setConfirmOpen(true)} />
+                <ConfirmDialog
+                    title="Delete task?"
+                    open={confirmOpen}
+                    setOpen={setConfirmOpen}
+                    onConfirm={() => { deleteTask(id) }}
+                >
+                    Task cannot be recovered!
+                 </ConfirmDialog>
             </div>
         </div>
     );
